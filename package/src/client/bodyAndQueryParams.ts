009ae4b73a744b03e27013bc069b1b1f7e921949
Reuben <reubendrummond@gmail.com>
2022-07-20 12:31:37 +1000

    experimenting with client fetcher and hook

---
+++ package/src/client/bodyAndQueryParams.ts
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+
+// these inferred from server
+interface User {
+  id: number;
+  name: string;
+}
+
+// GET
+interface GETApiUsers {
+  users: User[];
+}
+interface GETApiUsersId {
+  id: string;
+  user: User;
+}
+interface GETApiUsersUsername {
+  username: string;
+  user: User;
+}
+interface GETApiUsersIdFriends {
+  friends: User[];
+  user: User;
+}
+
+// POST
+type POSTApiUserBody = User;
+interface POSTApiUsers {
+  status: 201;
+  user: User;
+}
+type POSTApiUsersIdFriendsBody = User[];
+interface POSTApiUsersIdFriends {
+  status: 201;
+  friends: User[];
+}
+
+type GETResAndQuery<Res, QueryParams extends {} = {}> = {
+  response: Res;
+  query?: QueryParams;
+};
+
+type POSTResAndBodyAndQuery<
+  Res,
+  BodySchema extends {} = {},
+  QueryParams extends {} = {}
+> = {
+  response: Res;
+  body: BodySchema;
+  query?: QueryParams;
+};
+
+export type GETTypeMap = {
+  "/api/users": GETResAndQuery<GETApiUsers>;
+  "/api/users/id/[id]": never;
+  [_: `/api/users/id/${number}`]: GETResAndQuery<GETApiUsersId>;
+  "/api/users/username/[username]": never;
+  [_: `/api/users/username/${string}`]: GETResAndQuery<GETApiUsersUsername>;
+  "/api/users/id/[id]/friends": never;
+  [_: `/api/users/id/${number}/friends`]: GETResAndQuery<
+    GETApiUsersIdFriends,
+    { all?: boolean }
+  >;
+};
+
+export type POSTTypeMap = {
+  "/api/users": POSTResAndBodyAndQuery<POSTApiUsers, POSTApiUserBody>;
+  "/api/users/id/[id]/friends": never;
+  [_: `/api/users/id/${number}/friends`]: POSTResAndBodyAndQuery<
+    POSTApiUsersIdFriends,
+    POSTApiUsersIdFriendsBody
+  >;
+};
+
+const wrapper = {
+  get: async <Endpoint extends keyof GETTypeMap>(
+    endpoint: Endpoint,
+    options?: {
+      query?: GETTypeMap[Endpoint]["query"];
+    }
+  ): Promise<GETTypeMap[Endpoint]> => {
+    // append query
+    const response = await fetch(endpoint).then((res) => res.json());
+    return response;
+  },
+  post: async <Endpoint extends keyof POSTTypeMap>(
+    endpoint: Endpoint,
+    options: {
+      body: POSTTypeMap[Endpoint]["body"];
+      query?: POSTTypeMap[Endpoint]["query"];
+    }
+  ): Promise<GETTypeMap[Endpoint]> => {
+    // append query
+    const response = await fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify(options.body),
+    }).then((res) => res.json());
+    return response;
+  },
+};
+
+const getRes = wrapper.get("/api/users/id/123/friends");
+const getResWithQuery = wrapper.get("/api/users/id/123/friends", {
+  query: {
+    all: true,
+  },
+});
+const postRes = wrapper.post("/api/users", {
+  body: {
+    id: 123,
+    name: "my name",
+  },
+});
---
+++ package/src/client/experimentaion.ts
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+
+// these inferred from server
+interface User {
+  id: number;
+  name: string;
+}
+interface GETApiUsers {
+  users: User[];
+}
+interface GETApiUsersId {
+  id: string;
+  user: User;
+}
+interface GETApiUsersUsername {
+  username: string;
+  user: User;
+}
+interface GETApiUsersIdFriends {
+  friends: User[];
+  user: User;
+}
+
+// this generated
+export type GETTypeMap = {
+  "/api/users": GETApiUsers;
+  "/api/users/id/[id]": never;
+  [_: `/api/users/id/${number}`]: GETApiUsersId;
+  "/api/users/username/[username]": never;
+  [_: `/api/users/username/${string}`]: GETApiUsersUsername;
+  "/api/users/id/[id]/friends": never;
+  [_: `/api/users/id/${number}/friends`]: GETApiUsersIdFriends;
+};
+
+// wrapper and hook
+const fetchWrapper = async <Endpoint extends keyof GETTypeMap>(
+  endpoint: Endpoint
+): Promise<GETTypeMap[Endpoint]> => {
+  const response = await fetch(endpoint).then((res) => res.json());
+  return response;
+};
+
+interface Error {
+  message: string;
+}
+
+interface FetchHookSuccess<Res> {
+  isSuccess: true;
+  isError: false;
+  isLoading: false;
+  data: Res;
+  error: null;
+}
+interface FetchHookFail {
+  isSuccess: false;
+  isError: true;
+  isLoading: false;
+  data: null;
+  error: Error;
+}
+interface FetchHookLoading {
+  isSuccess: false;
+  isError: false;
+  isLoading: true;
+  data: null;
+  error: null;
+}
+type FetchHook<Res> = FetchHookSuccess<Res> | FetchHookFail | FetchHookLoading;
+
+const useFetch = <Endpoint extends keyof GETTypeMap>(endpoint: Endpoint) => {
+  const [data, setData] = useState<FetchHook<GETTypeMap[Endpoint]>>({
+    isSuccess: false,
+    isError: false,
+    isLoading: true,
+    data: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    fetchWrapper(endpoint).then((response) => {
+      setData({
+        ...data,
+        data: response,
+        isSuccess: true,
+        isLoading: false,
+        isError: false,
+        error: null,
+      });
+    });
+  });
+  return data;
+};
+
+const d = useFetch("/api/users/id/123/friends");
+
+if (d.isSuccess) {
+  d.data;
+}
